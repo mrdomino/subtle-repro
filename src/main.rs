878af6bb6caedcd8b8f0496a4bcd22810e675bb7
Steven Dee <git@wholezero.org>
2025-12-01 16:30:34 -0500

    Repro against crypto-bigint v0.6.1 (#5)
    Shows that release versions of all upstream dependencies exhibit the bug.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
-use chacha20::ChaCha8Rng;
-use crypto_bigint::{NonZero, RandomMod, Uint};
+use crypto_bigint::{NonZero, RandomBits, Uint};
+use rand_chacha::ChaCha8Rng;
 use rand_core::{RngCore, SeedableRng};
+use subtle::ConstantTimeLess;
 
+fn bad_random_mod<R>(rng: &mut R, n: &Uint<5>) -> Uint<5>
+where
+    R: RngCore,
+{
+    let n_bits = n.bits_vartime();
+    loop {
+        let x = Uint::random_bits(rng, n_bits);
+        if x.ct_lt(n).into() {
+            return x;
+        }
+    }
+}
 
 fn main() {
-    let mut rng  = ChaCha8Rng::seed_from_u64(1);
+    let mut rng = ChaCha8Rng::seed_from_u64(1);
     let special = rng.next_u64();
     let n = NonZero::new(Uint::<5>::ZERO.wrapping_sub(&Uint::from(special))).unwrap();
 
-    let a = Uint::<5>::random_mod(&mut rng, &n); // XXX HANGS
+    let a = bad_random_mod(&mut rng, &n);
 
     println!("Hello, {a:?}");
 }
